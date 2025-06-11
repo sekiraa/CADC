1eb3d919ae1511fe3476556bc40eb7f04c143cb2
sekiraa <almirnabiullin41470@gmail.com>
2025-06-11 17:22:43 +0300

    Alpha-0.0.1

--- CADC.py
+++ CADC.py
@@ -1028,7 +1028,7 @@ class FileViewer(QWidget, BaseEncodingClass):
         self.all_clear_button.hide()
         
         # Заменяем обычный QLabel на ScrollingLabel
-        self.label_status = ScrollingLabel("⏳ Ожидаетфыафыася действие.")
+        self.label_status = ScrollingLabel("⏳ Ожидание действие!")
         self.label_status.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.label_status.setFixedSize(378, 25)
         
