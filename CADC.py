f9d8292a2bef886fdef8ffba6ed281d5135fa0d5
sekiraa <almirnabiullin41470@gmail.com>
2025-06-11 16:07:49 +0300

    0.0.0

--- CADC.py
+++ CADC.py
@@ -23,7 +23,7 @@ def get_application_path():
         application_path = Path(__file__).resolve().parent
     return application_path
 
-# Определяем пут
+# Определяем пу
 directory_path = get_application_path()
 folder_path = directory_path / "list_CADC"
 
